a3e2dc5b6cb4f32cf746f6221e0cfaa648520ee0
Joel Martinez <jm8587700@gmail.com>
2024-04-13 14:49:51 -0300

    nada

--- frontend/src/ErrorRouter.jsx
+++ frontend/src/ErrorRouter.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate ,useEffect} from 'react-router-dom'
 
 export const ErrorRouter = () => {
+
+  
     const navegate = useNavigate()
     useEffect(() => {
     navegate("/singUp")
