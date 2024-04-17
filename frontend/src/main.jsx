1cd048db7e0b9a071243b1c92aa9a4bb97f98267
Joel Martinez <jm8587700@gmail.com>
2024-04-16 21:38:13 -0300

    creo que ya arregle el problema de la rutas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,53 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { Login } from "./formalariosRegistros/Login";
+import { SingUp } from "./formalariosRegistros/SingUp";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Verificar } from "./formalariosRegistros/Verificar";
+import { ProviderSIngUp } from "./Contextos/ProviderSIngUp";
+import { ProviderTareas } from "./Contextos/ProviderTareas";
+import { EstadoEliminarTarea } from "./Contextos/EstadoEliminarTarea";
+import { ProviderPasarIdTareaCompletada } from "./Contextos/ProviderPasarIdTareaCompletada";
+import { EstadosCrudNotas } from "./Contextos/EstadosCrudNotas";
+import { ProviderEjecutarRetomarTiempo } from "./Contextos/ProviderEjecutarRetomarTiempo";
+import { ErrorRouter } from "./ErrorRouter";
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: (
+      <ProviderSIngUp>
+        <Login />
+      </ProviderSIngUp>
+    ),
+  },
+  {
+    path: "/singUp",
+    element: <SingUp />,
+  },
+  {
+    path: "/app",
+    element: (
+      <ProviderSIngUp>
+        <Verificar />
+      </ProviderSIngUp>
+    ),
+    children: [
+      {
+        path: "/app",
+        element: (
+          <ProviderEjecutarRetomarTiempo>
+          <EstadosCrudNotas>
+            <ProviderPasarIdTareaCompletada>
+              <EstadoEliminarTarea>
+                <ProviderTareas>
+                  <App />
+                </ProviderTareas>
+              </EstadoEliminarTarea>
+            </ProviderPasarIdTareaCompletada>
+          </EstadosCrudNotas>
+          </ProviderEjecutarRetomarTiempo>
+        ),
+      },
+    ],
+  },
+  {
+    path: "*",
+    element:<ErrorRouter/>
+  },
+]);
 
-
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { App } from './App';
-import { Login } from './formalariosRegistros/Login';
-import { SingUp } from './formalariosRegistros/SingUp';
-import { Verificar } from './formalariosRegistros/Verificar';
-import { ProviderSIngUp } from './Contextos/ProviderSIngUp';
-import { ProviderTareas } from './Contextos/ProviderTareas';
-import { EstadoEliminarTarea } from './Contextos/EstadoEliminarTarea';
-import { ProviderPasarIdTareaCompletada } from './Contextos/ProviderPasarIdTareaCompletada';
-import { EstadosCrudNotas } from './Contextos/EstadosCrudNotas';
-import { ProviderEjecutarRetomarTiempo } from './Contextos/ProviderEjecutarRetomarTiempo';
-import { ErrorRouter } from './ErrorRouter';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={
-          <ProviderSIngUp>
-            <Login />
-          </ProviderSIngUp>
-        } />
-        <Route path="/singUp" element={<SingUp />} />
-        <Route path="/app" element={
-          <ProviderSIngUp>
-            <Verificar />
-          </ProviderSIngUp>
-        }>
-          <Route index element={
-            <ProviderEjecutarRetomarTiempo>
-              <EstadosCrudNotas>
-                <ProviderPasarIdTareaCompletada>
-                  <EstadoEliminarTarea>
-                    <ProviderTareas>
-                      <App />
-                    </ProviderTareas>
-                  </EstadoEliminarTarea>
-                </ProviderPasarIdTareaCompletada>
-              </EstadosCrudNotas>
-            </ProviderEjecutarRetomarTiempo>
-          } />
-        </Route>
-        <Route path="*" element={<ErrorRouter />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
