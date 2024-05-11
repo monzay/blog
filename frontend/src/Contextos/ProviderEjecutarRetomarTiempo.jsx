c9cd025f5d0ee3b4a36fb37a8f29c757c5e0bb92
Joel Martinez <jm8587700@gmail.com>
2024-05-11 16:03:22 -0300

    ns

--- frontend/src/Contextos/ProviderEjecutarRetomarTiempo.jsx
+++ frontend/src/Contextos/ProviderEjecutarRetomarTiempo.jsx
@@ -68,7 +68,7 @@ export const ProviderEjecutarRetomarTiempo = ({children}) => {
               mandarIdParaNoMastrarLasTareasCompletadas();
             }
           }
-        }, 1000);
+        }, 60000);
       }
       function retomamosElTiempo() {
         const tiempo = JSON.parse(localStorage.getItem("tiempoRestanTarea"));
--- frontend/src/web/ModelHoraDeLaTarea.jsx
+++ frontend/src/web/ModelHoraDeLaTarea.jsx
@@ -103,10 +103,11 @@ export const ModelHoraDeLaTarea = ({ data }) => {
           mandarIdParaNoMastrarLasTareasCompletadas();
         }
       }
-    }, 1000);
+    }, 60000);
 
     setIntervalRestarTiempo(interval);
   }
+
 
   //------------------------------------------------------------------------//
   //------------------------------------------------------------------------//
